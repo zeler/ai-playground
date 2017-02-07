5ceb0e667f6c001ead0fe59a75d5ac08143a4035
Michal Paprcka <michal.paprcka@gratex.com>
2017-02-07 17:15:11 +0100

    [DEV[ KNN algorithm tuning

--- classification/k_nearest_neighbors/terrain_classification/classifyKNN.py
+++ classification/k_nearest_neighbors/terrain_classification/classifyKNN.py
@@ -3,8 +3,8 @@ from sklearn.neighbors import KNeighborsClassifier
 
 def classify(features_train, labels_train):
 
-    clf = KNeighborsClassifier(algorithm='auto',
-                               n_neighbors=3,
+    clf = KNeighborsClassifier(algorithm='ball_tree',
+                               n_neighbors=11,
                                weights='distance')
     clf.fit(features_train, labels_train)
 
