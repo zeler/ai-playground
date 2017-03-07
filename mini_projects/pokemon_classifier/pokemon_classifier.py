9165d59ed6f8eb4d01c237b8dc75d78822014cb6
Michal Paprcka <michal.paprcka@gratex.com>
2017-03-07 21:56:44 +0100

    [DEV] added some data analysis

--- mini_projects/pokemon_classifier/pokemon_classifier.py
+++ mini_projects/pokemon_classifier/pokemon_classifier.py
@@ -5,12 +5,20 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn.feature_selection import f_classif, SelectKBest
+from sklearn.decomposition import PCA
 
 '''
     Simple Pokemon type classifier based on Pokemon stats.
-
-    TODO: maybe change algorithm, use less features...
 '''
+
+
+def doPCA(features):
+    pca = PCA(n_components=2)
+    pca.fit(features)
+
+    print(pca.explained_variance_ratio_)
+    print(pca.components_)
 
 df = pd.read_csv('Pokemon.csv')
 
@@ -18,7 +26,7 @@ df = pd.read_csv('Pokemon.csv')
 df = df.fillna('NA')
 
 # features to use
-cols = ['Total', 'HP', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed',
+cols = ['HP', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed',
         'Type 2']
 
 # Type 1 will be used for labels
@@ -43,12 +51,27 @@ label_encoder = LabelEncoder()
 train_features = feature_vectorizer.fit_transform(train_features)
 train_labels = label_encoder.fit_transform(train_labels)
 
+test_features = feature_vectorizer.transform(test_features)
+test_labels = label_encoder.transform(test_labels)
+
+# do PCA
+# doPCA(train_features)
+
+# Feature selector
+# selector = SelectKBest(f_classif, k=)
+# selector.fit(train_features, train_labels)
+
+# print(selector.scores_)
+
+# train_features = selector.transform(train_features)
+# test_features = selector.transform(test_features)
+
 # train model
 clf = RandomForestClassifier(n_estimators=50, min_samples_split=5)
 clf.fit(train_features, train_labels)
 
 # do prediction for test features
-predictions = clf.predict(feature_vectorizer.transform(test_features))
+predictions = clf.predict(test_features)
 
 # print accuracy score
-print(accuracy_score(label_encoder.transform(test_labels), predictions))
+print(accuracy_score(test_labels, predictions))
